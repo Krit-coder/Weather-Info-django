680dbf836586900945d42ff73613710f373517ea
Krit-coder <kritraj.01@gmail.com>
2023-04-01 22:03:59 +0530

    Initail Commit

--- WeatherInfo/Weather/views.py
+++ WeatherInfo/Weather/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+import os
 
 # Create your views here.
+def index(request):
+    cities = City.objects.all()
+
+link = os.getenv('API_LINK')
+url = link
+
+if request.method == 'POST':
+    form = CityForm(request.POST)
+    form.save()
+
+form = CityForm()
+
+weather_data = []
+
